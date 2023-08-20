0f8e5daeb886bbd626ae449eebd3e12defb6cd95
Bhakti J <jadhavbhakti80@gmail.com>
2023-08-20 20:49:29 +0530

    vercel 2

--- client/src/Components/LineChart.jsx
+++ client/src/Components/LineChart.jsx
@@ -15,7 +15,7 @@ export default function LineChart(){
     const [selectedMonth, setSelectedMonth] = useState('May-June');
 
     useEffect(() => {
-        axios.get('https://react-dashboard-bhaktij338.vercel.app/'+`${selectedMonth}`).then(response => {
+        axios.get(`${import.meta.env.VITE_API_BASE_URL}`+`${selectedMonth}`).then(response => {
           console.log(response);
           const fetchedData = response.data; 
           
